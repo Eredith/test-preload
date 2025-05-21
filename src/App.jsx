4c1a9862f339ac3d63f5ebe8a598704963d8eab7
Andreo Friendly <102529981+andreofriendly@users.noreply.github.com>
2025-05-22 01:01:25 +0700

    freakin case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import "./app.css"
 import "./index.css"
-import "./styles/Gameplay.css"
-import "./styles/ResultsScreen.css"
-import "./styles/RotationOverlay.css"
+import "./styles/gameplay.css"
+import "./styles/resultsscreen.css"
+import "./styles/rotationoverlay.css"
 import MainMenu from './components/MainMenu'
 import CharacterSelect from './components/CharacterSelect'
 import GameplayScene from './components/GameplayScene'
